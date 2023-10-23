98dd4cd3b253071d918dfb3698515410a5363a39
Ahmet Kök <kokahmet@pm.me>
2023-10-23 13:47:11 +0300

    first commit

---
+++ WebApi.BlazorClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WebApi.BlazorClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7079/api/") });
+
+await builder.Build().RunAsync();
+
---
+++ WebApi.WebApi/Controllers/CarsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.WebApi.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CarsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var cars = CarsContext.LuxuryCars.ToList();
+            return Ok(cars);
+        }
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            if (id == Guid.Empty)
+                return BadRequest("Id cannot be empty.");
+
+            var car = CarsContext.LuxuryCars.FirstOrDefault(x => x.Id == id);
+
+            if (car is null)
+                return NotFound("The car requested with given Id is not found.");
+
+            return Ok(car);
+
+        }
+    }
+}
+
